5d5a4118ade8130ac9fc0f08a5ab351800b2a953
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2022-12-16 11:11:32 -0500

    realizando deploy ng deploy --base-href=https://Jasr24.github.io/thecatapi/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: "https://api.thecatapi.com/v1",
+  claveApi: "live_32jyAKb9ojOzbE8a7H7eWGAHuD1PeRWhOwUHD74A8DD0B3n2yzX2hpmmUV2g7PuY"
 };
